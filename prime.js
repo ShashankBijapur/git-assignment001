5f9d3928a99eb2d72634638c1d79276e9401c1db
Shashank Bijapur <shashankcbijapur24@gmail.com>
2022-09-22 18:12:09 +0530

    prime.js

---
+++ prime.js
@@ -0,0 +1,17 @@
+function checkprime(num){
+    let count=0;
+    for (i=1;i<=num;i++){
+      if(num%i==0){
+        count++;
+      }
+    }
+    if(count==2){
+      return true;
+    }else {
+      return false;
+    }
+  }
+  checkprime(7);
+  checkprime(15);
+  
+  
